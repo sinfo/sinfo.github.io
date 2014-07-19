2670f606c251095d2a09e5d20178daf2cbeb7374
Diogo Pais <dpais11@gmail.com>
2014-07-19 22:50:07 +0100

    resume upload

---
+++ js/fileupload/fileupload.js
@@ -0,0 +1,27 @@
+$(function () {
+  "use strict";
+
+  var url = "http://resumes.bananamarket.eu/resume";
+
+  $("#fileupload").fileupload({
+    url: url,
+    dataType: "multipart/form-data",
+    progressall: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      $("#upload").text("Uploading: " + progress + "%");
+
+      if (progress === 100) {
+        $("#upload").text("Upload complete.");
+      }
+    },
+    add: function (e, data) {
+      $("#upload").removeClass("red");
+      $("#upload").addClass("green");
+      $("#upload").text("Upload " + data.files[0].name);
+      $("#upload").click(function() {
+        data.submit();
+      });
+      $("#upload").show();
+    }
+  });
+});

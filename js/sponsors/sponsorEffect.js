46b5ffa33df9e813da60aeb5df27e55620f94b0a
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2014-10-23 18:35:00 +0100

    Update toggle effect And price colors

--- js/sponsors/sponsorEffect.js
+++ js/sponsors/sponsorEffect.js
@@ -4,16 +4,26 @@ jQuery(document).ready(function() {
 
 	//toggle the componenet with class msg_body
 	jQuery(".heading").click(function(){
-	 	jQuery(this).next(".content").slideToggle(500);
 	 	jQuery(this).next(".item").next(".description").show();
+
+	 	if (jQuery(this).next(".content").is(":hidden")) {
+	 		jQuery(this).closest('div').find(".toggle").text("-");
+	 		jQuery(this).next(".content").slideToggle(500);	 		
+	 	}else{
+	 		jQuery(this).closest('div').find(".toggle").text("+");
+	 		jQuery(this).next(".content").slideToggle(500);	 
+	 	};
+
 	});
 
 	jQuery(".item").click(function(){
 	 	if (jQuery(this).next(".description").is(":hidden")) {
 	 		jQuery(".description").hide();
 	 		jQuery(this).next(".description").show();
+	 		jQuery(this).closest(".toggle").text("-");
 	 	}else {
 	 		jQuery(this).next(".description").hide();
+	 		jQuery(this).closest('div').next.find(".toggle").text("+");
 	 	};               
 	});
 

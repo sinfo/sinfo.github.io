cde1fe868724ee6a2acce6a0af873d9d5f8250f6
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2014-10-31 03:53:31 +0000

    Fixed bug on scrolling

--- js/sponsors/sponsorEffect.js
+++ js/sponsors/sponsorEffect.js
@@ -31,7 +31,9 @@ jQuery(document).ready(function() {
 	});
 
 	jQuery(".redirectPub").click(function(){
-		$( ".container" ).find( ".heading" ).next(".content").eq(2).slideToggle(500);
+		if ($( ".container" ).find( ".heading" ).next(".content").eq(2).is(":hidden")) {
+			$( ".container" ).find( ".heading" ).next(".content").eq(2).slideToggle(500);
+		};
 		$('html, body').animate({
 	        scrollTop: $("#Publicity").offset().top
 	    }, 2000);
